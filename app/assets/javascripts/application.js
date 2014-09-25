5bdd3404bff8471a8deb1fd8a4c780f514f8c92b
Dominik Masur <dominik.masur@googlemail.com>
2014-09-25 20:11:14 +0200

    removed angular resource

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
-//= require gmaps/google
 //= require underscore
 //= require angular/angular
 //= require angular-cookie/angular-cookie
