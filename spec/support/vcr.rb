8b5662e3cf4baeeebd33545b6d0793141a8f4cc7
Dominik Masur <dominik.masur@gmail.com>
2014-11-21 22:33:18 +0100

    ignoring codeclimate

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -4,5 +4,6 @@ VCR.configure do |c|
   c.cassette_library_dir = Rails.root.join 'spec', 'vcr'
   c.hook_into :webmock
   c.ignore_localhost = true
+  c.ignore_hosts 'codeclimate.com'
   c.configure_rspec_metadata!
 end
