7571747708a2cd1f1b7f1cb1a4d80dcfafcc5ce3
Dominik Masur <dominik.masur@gmail.com>
2015-02-23 23:28:33 +0100

    fix geo

--- spec/support/phantomjs_ext/geolocation.js
+++ spec/support/phantomjs_ext/geolocation.js
@@ -1,10 +1,10 @@
-window.geolocation =
-{
-  getLocation: function() {
-    return {
-      then: function(callback) {
-        callback({ coords: { latitude: "51.4317", longitude: "7" } });
-      }
-    }
-  }
-}
+// window.geolocation =
+// {
+//   getLocation: function() {
+//     return {
+//       then: function(callback) {
+//         callback({ coords: { latitude: "51.4317", longitude: "7" } });
+//       }
+//     }
+//   }
+// }
